9cd84b95b27bcad22daf3adf9592e756de4e721f
Dhara Santos <santtosdhara@gmail.com>
2025-01-23 12:25:41 -0500

    New initial commit

---
+++ BlueberryTimer/BlueberryTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlueberryTimer
+//
+//  Created by Dhara Santos on 1/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
