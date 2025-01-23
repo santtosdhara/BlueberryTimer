3f4bb92aa18d87d9e356613366a1594eed7dab55
Dhara Santos <santtosdhara@gmail.com>
2025-01-23 12:25:41 -0500

    New initial commit

---
+++ BlueberryTimer/BlueberryTimer/BlueberryTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueberryTimerApp.swift
+//  BlueberryTimer
+//
+//  Created by Dhara Santos on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BlueberryTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
