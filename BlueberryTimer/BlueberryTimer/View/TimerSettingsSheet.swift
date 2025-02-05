4f9e7ec8fb411104a7de6892e3b7449c8ba6786a
Dhara Santos <santtosdhara@gmail.com>
2025-02-04 21:29:53 -0500

    feature #28: Create TimerSettingSheet
    Created the scratch of the setting page sheet, the user can already set the timer using it but it's just the first versio.

---
+++ BlueberryTimer/BlueberryTimer/View/TimerSettingsSheet.swift
@@ -0,0 +1,18 @@
+//
+//  TimerSettingsSheet.swift
+//  BlueberryTimer
+//
+//  Created by Dhara Santos on 2/4/25.
+//
+
+import SwiftUI
+
+struct TimerSettingsSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerSettingsSheet()
+}
